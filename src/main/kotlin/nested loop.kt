c0e985156f7262887115968b666bbd15e05608c6
richi98 <richibarua06@email.com>
2023-11-29 13:45:05 +0600

    pyramid

--- src/main/kotlin/nested loop.kt
+++
@@ -1,7 +0,0 @@
-fun main(array: Array<String>){
-    for (i in 1  ..5){
-        for (j in 1 .. 5){
-            println("$i $j")
-        }
-    }
-}
---
+++ src/main/kotlin/pyramid.kt
@@ -0,0 +1,9 @@
+fun main(array: Array<String>){
+    val row = 5
+    for (i in 1 .. row){
+        for (j in 1 .. i){
+            print("* ")
+        }
+        println()
+    }
+}
